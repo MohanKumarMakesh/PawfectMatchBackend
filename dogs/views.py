35b8dcdddc0dda1cde9003370f990bc461a38a23
MohanKumarMakesh <x23377852@student.ncirl.ie>
2025-03-18 12:49:29 +0000

    update

--- dogs/views.py
+++ dogs/views.py
@@ -12,11 +12,13 @@ from .serializers import DogSerializer
 # Set up logging
 logger = logging.getLogger(__name__)
 
+
 @api_view(['GET'])
 def get_dogs(request):
     dogs = Dog.objects.all()
     serializer = DogSerializer(dogs, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 def add_dog(request):
@@ -27,28 +29,6 @@ def add_dog(request):
     try:
         if image.size > (1024 * 1024):  # Check if image size is greater than 1 MB
             img = Image.open(image)
-<<<<<<< HEAD
-            if img.size > (1024 * 1024):  # Check if image size is greater than 1 MB
-                output = io.BytesIO()
-                img.save(output, format='JPEG', quality=85)
-                output.seek(0)
-                image = output
-            print(settings.AWS_STORAGE_BUCKET_NAME)
-            s3 = boto3.client('s3')
-            s3.upload_fileobj(
-                image, settings.AWS_STORAGE_BUCKET_NAME, f'dogs/{image.name}')
-            image_url = f'https://{settings.AWS_S3_CUSTOM_DOMAIN}/dogs/{image.name}'
-            serializer.validated_data['image'] = image_url
-
-        serializer.save()
-        # Fetch the updated list of dogs
-        dogs = Dog.objects.all()
-        updated_serializer = DogSerializer(dogs, many=True)
-        return Response(updated_serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        logger.error(f"Serializer errors: {serializer.errors}")
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
             output = io.BytesIO()
             img.save(output, format='JPEG', quality=85)
             output.seek(0)
@@ -75,7 +55,8 @@ def add_dog(request):
             return Response(updated_serializer.data, status=status.HTTP_201_CREATED)
         else:
             # Remove the uploaded image from S3 if data is not valid
-            s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f'dogs/{image.name}')
+            s3.delete_object(
+                Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f'dogs/{image.name}')
             logger.error(f"Serializer errors: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
@@ -113,19 +94,22 @@ def add_dog(request):
             return Response(updated_serializer.data, status=status.HTTP_201_CREATED)
         else:
             # Remove the uploaded image from S3 if data is not valid
-            s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f'dogs/{image.name}')
+            s3.delete_object(
+                Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f'dogs/{image.name}')
             logger.error(f"Serializer errors: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         logger.error(f"Error uploading image: {str(e)}")
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+
+
 @api_view(['DELETE'])
 def delete_dog(request, dog_id):
     try:
         dog = Dog.objects.get(id=dog_id, user=request.user)
         s3 = boto3.client('s3')
-        s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=f'dogs/{dog.image.split("/")[-1]}')
+        s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+                         Key=f'dogs/{dog.image.split("/")[-1]}')
         dog.delete()
         return Response({"message": "Dog and image deleted successfully"}, status=status.HTTP_200_OK)
     except Dog.DoesNotExist:
@@ -133,4 +117,3 @@ def delete_dog(request, dog_id):
     except Exception as e:
         logger.error(f"Error deleting dog: {str(e)}")
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
->>>>>>> 81c0dc896250eca6fae3d8b845e58284410d800b
